9e3456226c4f74fbd0d822d6c79d7439e8a2fbec
Morqulis <morqqulis@gmail.com>
2024-08-27 04:56:47 +0400

    can't fix warning about _id: buffer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import DefaultLayout from '#layouts/DefaultLayout'
 import HomeSection from '#sections/Home/HomeSection'
+import { Suspense } from 'react'
+
+export const experimental_ppr = true
+
 const Home = async () => {
    return (
       <DefaultLayout full>
          <main>
-            <HomeSection />
+            <Suspense
+               fallback={
+                  <div className={`grid h-full place-items-center text-center text-4xl text-tomato-300`}>
+                     Loading...
+                  </div>
+               }
+            >
+               <HomeSection />
+            </Suspense>
          </main>
       </DefaultLayout>
    )
--- src/components/sections/Home/HomeSection.tsx
+++ src/components/sections/Home/HomeSection.tsx
@@ -1,14 +1,16 @@
 import axios from 'axios'
 import ProductsSection from './ProductsSection'
 
+export const revalidate = 0
+
 const HomeSection: React.FC = async (): Promise<JSX.Element> => {
    const result = await axios.get('http://localhost:3000/api/products')
-   const serializedProducts = JSON.parse(JSON.stringify(await result.data))
+   const serialisedResult = JSON.parse(JSON.stringify(await result.data))
 
    return (
       <div className="container flex flex-col gap-10">
-         <ProductsSection title={'New Products'} products={serializedProducts} />
-         <ProductsSection title={'All Products'} products={serializedProducts} />
+         <ProductsSection title={'New Products'} products={serialisedResult} />
+         <ProductsSection title={'All Products'} products={serialisedResult} />
       </div>
    )
 }
