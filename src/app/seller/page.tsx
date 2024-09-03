699555aa24fffd51a0405c333d6ac0ad73212fff
BayramBehbudov <bayram.behbudov@gmail.com>
2024-09-03 12:08:20 +0400

    update

--- src/app/seller/page.tsx
+++ src/app/seller/page.tsx
@@ -1,5 +1,5 @@
 import DefaultLayout from '#layouts/DefaultLayout'
-import SellerHome from '#pages/Seller/Home/SellerHome'
+import SellerHome from '#sections/Seller/Home/SellerHome'
 
 const Seller: React.FC = (): JSX.Element => {
    return (
